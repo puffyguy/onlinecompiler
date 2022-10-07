2040edb34b3ea42b16ed4e881b4a28e2058564f1
prafuln <prafuln.mkcl.org>
2022-10-07 18:27:46 +0530

    initial steps

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import axios from 'axios'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+var baseURL = "/server"
+axios.defaults.baseURL = baseURL
 
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/server': {
+        target: 'http://localhost:8000/',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/server': ''
+        }
+      },
+    }
+  }
 })
